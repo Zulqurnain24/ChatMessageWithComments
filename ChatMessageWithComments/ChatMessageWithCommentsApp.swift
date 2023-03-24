2dd23cd5669ddcbe50e5184b9720c7a13cbc1c83
Muhammad Zulqurnain <muhammad@kalido.me>
2023-03-24 21:27:21 +0000

    Initial Commit

---
+++ ChatMessageWithComments/ChatMessageWithCommentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMessageWithCommentsApp.swift
+//  ChatMessageWithComments
+//
+//  Created by Muhammad Zulqurnain on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatMessageWithCommentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
